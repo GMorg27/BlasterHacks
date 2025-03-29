31ceca535c2ca06743c484fefed9d5efaf21773c
Geoffrey Morgan <geoffrey.morgan1200@gmail.com>
2025-03-28 22:12:11 -0600

    Backend initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BlasterHacks
+## Backend
+```export MONGODB_URL="mongodb+srv://<username>:<password>@<url>/<db>?retryWrites=true&w=majority"```
+
+```cd backend```
+
+```uvicorn app:app --reload```
---
+++ backend/models.py
@@ -0,0 +1,46 @@
+from typing import Optional
+
+from typing_extensions import Annotated
+
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.functional_validators import BeforeValidator
+
+# Represents an ObjectId field in the database.
+# It will be represented as a `str` on the model so that it can be serialized to JSON.
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+
+class UserModel(BaseModel):
+    """
+    Container for a user record.
+    """
+    # The primary key for the UserModel, stored as a `str` on the instance.
+    # This will be aliased to `_id` when sent to MongoDB,
+    # but provided as `id` in the API requests and responses.
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    name: str = Field(...)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={
+            "example": {
+                "name": "Jane Doe",
+            }
+        },
+    )
+
+
+class UpdateUserModel(BaseModel):
+    """
+    Container for a user record.
+    """
+    name: Optional[str] = None
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={
+            "example": {
+                "name": "Jane Doe",
+            }
+        },
+    )
