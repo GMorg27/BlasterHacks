c95acb78008d26eb46927979b42bdd6a2688d788
Geoffrey Morgan <geoffrey.morgan1200@gmail.com>
2025-03-29 13:14:01 -0600

    Assignment list display

--- frontend/src/accessors/assignmentAccessor.js
+++ frontend/src/accessors/assignmentAccessor.js
@@ -1,3 +1,28 @@
+// GET request to get all the assingments for a given user
+export async function getUserAssignments(username) {
+    const queryParam = "?username=" + username;
+    const URL = "http://localhost:8000/assignments/" + queryParam;
+    try {
+        const response = await fetch(URL, {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            },
+        });
+        
+        if (!response.ok) {
+            throw new Error("Failed to retrieve user assignments");
+        }
+    
+        const data = await response.json();
+        console.log("Success:", data);
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
+}
+
 // POST request to send an ICS string to the backend for a user
 export async function uploadICSString(icsString, username) {
     const queryParam = "?username=" + username;
